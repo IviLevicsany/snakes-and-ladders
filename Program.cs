af3d18eb73444fde26163d6689b2f66046046b21
IviLevicsany <ivan.levente.robert@csany-zeg.hu>
2025-10-15 07:47:51 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -13,14 +13,14 @@ namespace Snakes_and_ladders
         int hely = 1;
         public Program()
         {
-            Jatek();
+            Console.WriteLine(Letra(12));
         }
 
-        public void Kigyo()
+        public void Kigyo(int i)
         {
-            int[] kigyo_hely = {34, 16, 90, 67, 23};
-            int[] kigyo_ertek = {8, 6, 10, 12, 5};
-            for (int i = 0; i < palya; i++)
+            int[] kigyo_hely = {34,16,90,67,23};
+            int[] kigyo_ertek = {8,6,10,12,5};
+            for (int i = 0; i < 5; i++)
             {
                 if (hely == kigyo_hely[i])
                 {
@@ -28,20 +28,34 @@ namespace Snakes_and_ladders
                 }
             }
         }
-        public void Letra()
+        /*public void Letra(int i)
         {
-            int[] kigyo_hely = { 53, 26, 71, 86, 12 };
-            int[] kigyo_ertek = { 11, 3, 15, 6, 9 };
-            for (int i = 0; i < palya; i++)
+            int[] letra_hely = {53,26,71,86,12};
+            int[] letra_ertek = {11,3,15,6,9};
+            foreach (int n in letra_hely)
+                if (i.)
             {
-                if (hely == kigyo_hely[i])
-                {
-                    hely += kigyo_ertek[i];
-                }
+                hely += letra_ertek[i];
             }
         }
+        */
+        public int Letra(int i)
+        {
+            int[] letra_hely = { 53, 26, 71, 86, 12 };
+            int[] letra_ertek = { 11, 3, 15, 6, 9 };
 
-        
+            for (int index = 0; index < letra_hely.Length; index++)
+            {
+                if (i == letra_hely[index])
+                {
+                    i += letra_ertek[index];
+                    break; // megtaláltuk, nem kell tovább keresni
+                }
+            }
+
+            return i;
+        }
+
 
         public void Jatek()
         {
