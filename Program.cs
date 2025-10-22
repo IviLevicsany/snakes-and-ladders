f98502eabc41a15c54c829fd24139e3ef7703594
Creipy05 <99033830+Creipy05@users.noreply.github.com>
2025-10-22 08:23:13 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,67 +10,74 @@ namespace Snakes_and_ladders
     {
         const int palya = 100;
         Random rnd = new Random();
-        int hely = 1;
+        int p1_hely = 0;
+        int p2_hely = 0;
         public Program()
         {
-            Console.WriteLine(Letra(12));
+            //Console.WriteLine(Kigyo(67));
+            Jatek();
         }
 
-        public void Kigyo(int i)
+        public int Kigyo(int h)
         {
-            int[] kigyo_hely = {34,16,90,67,23};
-            int[] kigyo_ertek = {8,6,10,12,5};
-            for (int i = 0; i < 5; i++)
+            int[] kigyo_hely = { 34, 16, 90, 67, 23 };
+            int[] kigyo_ertek = { 8, 6, 10, 12, 5 };
+            for (int i = 0; i < kigyo_hely.Length; i++)
             {
-                if (hely == kigyo_hely[i])
+                if (h == kigyo_hely[i])
                 {
-                    hely -= kigyo_ertek[i];
+                    h -= kigyo_ertek[i];
+                    Console.WriteLine($"Kígyóra léptél, lecsúsztál {kigyo_ertek[i]} helyet!");
+                    break;
                 }
             }
+            return h;
         }
-        /*public void Letra(int i)
-        {
-            int[] letra_hely = {53,26,71,86,12};
-            int[] letra_ertek = {11,3,15,6,9};
-            foreach (int n in letra_hely)
-                if (i.)
-            {
-                hely += letra_ertek[i];
-            }
-        }
-        */
-        public int Letra(int i)
+        public int Letra(int h)
         {
             int[] letra_hely = { 53, 26, 71, 86, 12 };
             int[] letra_ertek = { 11, 3, 15, 6, 9 };
-
-            for (int index = 0; index < letra_hely.Length; index++)
+            for (int i = 0; i < letra_hely.Length; i++)
             {
-                if (i == letra_hely[index])
+                if (h == letra_hely[i])
                 {
-                    i += letra_ertek[index];
-                    break; // megtaláltuk, nem kell tovább keresni
+                    h += letra_ertek[i];
+                    Console.WriteLine($"Létrára léptél, felmásztál {letra_ertek[i]} helyet!");
+                    break;
                 }
             }
-
-            return i;
+            return h;
         }
-
 
         public void Jatek()
         {
-            int dobas = rnd.Next(1, 13);
-            while (hely < palya)
+            
+            while (p1_hely < palya && p2_hely < palya)
             {
-                hely += dobas;
-                if (hely > palya)
+                int dobas1 = rnd.Next(1, 13);
+                int dobas2 = rnd.Next(1, 13);
+                p1_hely += dobas1;
+                p2_hely += dobas2;
+                Kigyo(p1_hely);
+                Kigyo(p2_hely);
+                Letra(p1_hely);
+                Letra(p2_hely);
+                Console.WriteLine("1. játékos helye: " + p1_hely);
+                Console.WriteLine("2. játékos helye: " + p2_hely);
+                if (p1_hely >= palya)
                 {
-                    hely -= dobas;
+                    p1_hely = 100;
+                    Console.WriteLine("1. játékos nyert!", ConsoleColor.Yellow);
                 }
-                Kigyo();
-                Letra();
-                Console.WriteLine("A te helyed: " + hely);
+                else if (p2_hely >= palya)
+                {
+                    p2_hely = 100;
+                    Console.WriteLine("2. játékos nyert!", ConsoleColor.Yellow);
+                }
+                Console.WriteLine();
+                Console.ReadKey(intercept: true);
             }
+            
 
         }
         static void Main(string[] args)
